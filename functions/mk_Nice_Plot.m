252c6b6728bbaf78905b12f3df3b3ed50debbee9
Sara Steele <sara.steele14@gmail.com>
2016-02-18 13:54:03 -0500

    functions

--- functions/mk_Nice_Plot.m
+++ functions/mk_Nice_Plot.m
@@ -1,38 +1,37 @@
 %% function mk_Nice_Plot(h_title,h_xlabel,h_ylabel,h_legend)
 
 function mk_Nice_Plot(varargin)
-
+fs = 20;
 line = get(gca, 'Children');
 set(line,'LineWidth',2)
 %set(gca,'box','off')
 
     set(gca,'FontName','Arial', ...
-    'FontSize',18, ...
-    'FontWeight','bold', ...
+    'FontSize',fs, ...
     'LineWidth',2);
-    
+     %'FontWeight','bold', ...
 set(gca,'box','off')
 if nargin>=1
         
         h_t = varargin{1};
     set(h_t,'FontName','Arial', ...
-    'FontSize',16, ...
-    'FontWeight','bold');
+    'FontSize',fs);%, ...
+  %  'FontWeight','bold');
 end
 
 if nargin >=2
     h_x = varargin{2};        
     set(h_x,'FontName','Arial', ...
-    'FontSize',16, ...
-    'FontWeight','bold');
+    'FontSize',fs);%, ...
+   % 'FontWeight','bold');
 end
 
 if nargin >=3
 
     h_y = varargin{3};
     set(h_y,'FontName','Arial', ...
-    'FontSize',16, ...
-    'FontWeight','bold');
+    'FontSize',fs);%, ...
+   % 'FontWeight','bold');
     
 
                       
@@ -41,7 +40,7 @@ end
 if nargin >=4
     h_l = varargin{4};
     set(h_l,'FontName','Arial', ...
-    'FontSize',16, ...    
+    'FontSize',fs, ...    
     'FontWeight','normal');
 end
 
